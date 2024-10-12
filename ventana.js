33854bfca34643f3b1a4d05ffa179af9c40724cd
RayoJDmaga <mugrosojosue7@gmail.com>
2024-10-12 17:14:09 -0600

    Add files via upload

---
+++ ventana.js
@@ -0,0 +1,171 @@
+function Cambiar(event) {
+    event.preventDefault();
+    document.body.classList.toggle("borrar");
+    setTimeout(() => {
+        window.location.href = event.target.href;
+    }, 500);
+}
+//Funciones de ventanas equipoPC
+function Moni() {
+document.getElementById('moni').style.display='block';
+}
+function Moni_cerrar() {
+document.getElementById('moni').style.display = 'none';
+}
+function Te() {
+document.getElementById('te').style.display='block';
+}
+function Te_cerrar() {
+document.getElementById('te').style.display = 'none';
+}
+function Mou() {
+document.getElementById('mou').style.display='block';
+}
+function Mou_cerrar() {
+document.getElementById('mou').style.display = 'none';
+}
+function Ga() {
+document.getElementById('ga').style.display='block';
+}
+function Ga_cerrar() {
+document.getElementById('ga').style.display = 'none';
+}
+function Tar() {
+document.getElementById('tar').style.display='block';
+}
+function Tar_cerrar() {
+document.getElementById('tar').style.display = 'none';
+}
+function Dis() {
+document.getElementById('dis').style.display='block';
+}
+function Dis_cerrar() {
+document.getElementById('dis').style.display = 'none';
+}
+function Mem() {
+document.getElementById('mem').style.display='block';
+}
+function Mem_cerrar() {
+document.getElementById('mem').style.display = 'none';
+}
+function Memro() {
+document.getElementById('memro').style.display='block';
+}
+function Memro_cerrar() {
+document.getElementById('memro').style.display = 'none';
+}
+function Disco() {
+document.getElementById('disco').style.display='block';
+}
+function Disco_cerrar() {
+document.getElementById('disco').style.display = 'none';
+}
+function Red() {
+document.getElementById('red').style.display='block';
+}
+function Red_cerrar() {
+document.getElementById('red').style.display = 'none';
+}
+function Bo() {
+document.getElementById('bo').style.display='block';
+}
+function Bo_cerrar() {
+document.getElementById('bo').style.display = 'none';
+}
+function Im() {
+document.getElementById('im').style.display='block';
+}
+function Im_cerrar() {
+document.getElementById('im').style.display = 'none';
+}
+//funciones de ventanas equipo-laptop
+function A() {
+document.getElementById('a').style.display='block';
+}
+function A_cerrar() {
+document.getElementById('a').style.display = 'none';
+}
+function B() {
+document.getElementById('b').style.display='block';
+}
+function B_cerrar() {
+document.getElementById('b').style.display = 'none';
+}
+function C() {
+document.getElementById('c').style.display='block';
+}
+function C_cerrar() {
+document.getElementById('c').style.display = 'none';
+}
+function D() {
+document.getElementById('d').style.display='block';
+}
+function D_cerrar() {
+document.getElementById('d').style.display = 'none';
+}
+function E() {
+document.getElementById('e').style.display='block';
+}
+function E_cerrar() {
+document.getElementById('e').style.display = 'none';
+}
+function F() {
+document.getElementById('f').style.display='block';
+}
+function F_cerrar() {
+document.getElementById('f').style.display = 'none';
+}
+function G() {
+document.getElementById('g').style.display='block';
+}
+function G_cerrar() {
+document.getElementById('g').style.display = 'none';
+}
+function H() {
+document.getElementById('h').style.display='block';
+}
+function H_cerrar() {
+document.getElementById('h').style.display = 'none';
+}
+function I() {
+document.getElementById('i').style.display='block';
+}
+function I_cerrar() {
+document.getElementById('i').style.display = 'none';
+}
+function J() {
+document.getElementById('j').style.display='block';
+}
+function J_cerrar() {
+document.getElementById('j').style.display = 'none';
+}
+function K() {
+document.getElementById('k').style.display='block';
+}
+function K_cerrar() {
+document.getElementById('k').style.display = 'none';
+}
+function L() {
+document.getElementById('l').style.display='block';
+}
+function L_cerrar() {
+document.getElementById('l').style.display = 'none';
+}
+function M() {
+document.getElementById('m').style.display='block';
+}
+function M_cerrar() {
+document.getElementById('m').style.display = 'none';
+}
+function N() {
+document.getElementById('n').style.display='block';
+}
+function N_cerrar() {
+document.getElementById('n').style.display = 'none';
+}
+function O() {
+document.getElementById('o').style.display='block';
+}
+function O_cerrar() {
+document.getElementById('o').style.display = 'none';
+}
