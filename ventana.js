f8cbda692f1953af832a98e9195daf60f4c7a8ac
RayoJDmaga <mugrosojosue7@gmail.com>
2024-11-18 19:11:14 -0600

    Update ventana.js

--- ventana.js
+++ ventana.js
@@ -169,9 +169,3 @@ document.getElementById('o').style.display='block';
 function O_cerrar() {
 document.getElementById('o').style.display = 'none';
 }
-document.addEventListener("DOMContentLoaded", () => {
-    var numRandom = Math.floor(Math.random() * 21) + 1;
-    const NombreIMG = `compu2.${numRandom}.png`;
-    const imgElement = document.getElementById("DCurioso");
-    imgElement.src = NombreIMG;
-})
